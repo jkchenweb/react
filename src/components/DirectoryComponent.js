704734ad67880740f3e1257bec93b8e0d66c77f7
Jeff Chen <jkchenweb@gmail.com>
2020-10-17 11:35:27 -0700

    updated file location for campsites.js

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -47,11 +47,7 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
+
             </div>
         );
     }
