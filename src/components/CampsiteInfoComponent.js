cfea22881d5bb4bfef0f44ad7897588a27b42242
Jeff Chen <jkchenweb@gmail.com>
2020-10-22 00:03:59 -0700

    Revert "React Week 2 Workshop Assignment"
    This reverts commit cbaf290f208712ee20dc22e4afc6ab9733281c26.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -16,41 +16,17 @@ class CampsiteInfo extends Component {
             </div>
         )
     }
-
-    renderComments(comments){
-        if(comments){
-            return (
-                <div className='col-md-5 m-1'>
-                    <h4>Comments</h4>
-                    {
-                        comments.map( comment => {
-                            return(
-                            <div key={comment.id}>
-                            <p>
-                                {comment.text}<br/>
-                                -- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                            </p>   
-                            </div>
-                            )
-                        })
-                    }
-                </div>
-            )
-        }
-    }
-
     render(){
         if (this.props.campsite){
             return (
                 <div className="row">
                     {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}                    
                 </div>
             );
         }
         return (
             <div>
-
+                
             </div>
         )
         
