a59ef102889e7f0aa2211c0688d385774c1277df
Jeff Chen <jkchenweb@gmail.com>
2020-10-23 20:56:55 -0700

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -16,20 +16,39 @@ class CampsiteInfo extends Component {
             </div>
         )
     }
+    renderComments(comments){
+        if(comments){
+            return (
+                <div className='col-md-5 m-1'>
+                    <h4>Comments</h4>
+                    {
+                        comments.map( comment => {
+                            return(
+                            <div key={comment.id}>
+                            <p>
+                                {comment.text}<br/>
+                                -- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                            </p>   
+                            </div>
+                            )
+                        })
+                    }
+                </div>
+            )
+        }
+    }
     render(){
         if (this.props.campsite){
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                </div>
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>    
             );
         }
-        return (
-            <div>
-                
-            </div>
-        )
-        
+        return <div />;
     }
 }
 
